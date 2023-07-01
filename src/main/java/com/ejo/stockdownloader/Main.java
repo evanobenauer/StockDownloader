cfc2469bc2b71d1789c27270c0107bb4b1e00cc5
Evan Obenauer <evanjohnobenauer@gmail.com>
2023-07-01 18:20:01 -0400

    Cleaned Main Class

--- src/main/java/com/ejo/stockdownloader/Main.java
+++ src/main/java/com/ejo/stockdownloader/Main.java
@@ -21,21 +21,9 @@ public class Main {
             new Vector(800,600),
             new TitleScene(), true,4,60,60);
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         window.run();
         window.close();
-    }
-
-
-    public static void downloadCSV(String stockTicker, String timeSeries, String interval, String outputSize, String slice, String apiKey) throws IOException {
-        String folderPath = "stock_history/" + stockTicker + "/" + interval + "/";
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        String url = "https://www.alphavantage.co/query?function=" + timeSeries + "&symbol=" + stockTicker + "&interval=" + interval + "&slice=" + slice + "&outputsize=" + outputSize + "&adjusted=true&apikey=" + apiKey + "&datatype=csv" + "&adjusted=false";
-        System.out.println(url);
-        HttpGet request = new HttpGet(url);
-
-        CloseableHttpResponse response = httpClient.execute(request);
-
     }
 
 }
